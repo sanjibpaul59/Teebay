4b52388158b5552d496495b3daa43ff82623254b
sanjibpaul59 <sanjibpaul171@gmail.com>
2022-01-11 19:02:49 +0600

    added cors and rest_framework settings

--- teebaydmin/settings.py
+++ teebaydmin/settings.py
@@ -54,6 +54,16 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000'
 ]

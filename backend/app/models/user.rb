3e39b677378088a906844b7b624bd0741200d0b9
sanjibpaul59 <sanjibpaul171@gmail.com>
2023-03-16 23:09:59 +0600

    routes configuration with login sessions and user model update

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
  has_many :products
+ # validates: email, presence: true, uniqueness: true
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :transactions
   resources :product_categories
   resources :categories
   resources :products
-  resources :users
+  resources :users do
+    get 'products', to: 'users#products'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

d0693a332fd594631a3baf3d1bc7d9ae38875853
sanjibpaul59 <sanjibpaul171@gmail.com>
2023-03-18 00:34:03 +0600

    Tried to store user in sessions through middleware

--- client/services/userService.tsx
+++ client/services/userService.tsx
@@ -1,3 +1,5 @@
-export default async function login(email: string, password: string) {
- 
+import axios from "axios";
+export const getAuthUser = async () => {
+  const response = await axios.get("http://localhost:3000/current_user");
+  return response.data;
 }

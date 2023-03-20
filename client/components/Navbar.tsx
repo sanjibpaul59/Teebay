e83c152cfd811cec4da3d22d6a28c285a950316f
sanjibpaul59 <sanjibpaul171@gmail.com>
2023-03-21 02:54:49 +0600

    Added a Navbar component for better navigation

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,17 +1,24 @@
-import { Navbar, NavLink, NavLinkProps } from "@mantine/core";
+import {Navbar, Flex} from '@mantine/core'
+import Link from 'next/link'
+import { getCurrentUser } from '@/lib/getCurrentUser'
 
-export default function NavbarMinimal() {
+export default function Navigation() {
+  const authenticatedUser = getCurrentUser()
   return (
-    <Navbar color="gray" style={{ zIndex: 1 } }>
-      <NavLink component="a" href="/" color="gray" style={{ fontWeight: "bold" }}>
-        Tebaay
-      </NavLink>
-      <NavLink component="a" href="/products" color="gray">
-        Products
-      </NavLink>
-      <NavLink component="a" href="/auth" color="gray">
-        Login
-      </NavLink>
-    </Navbar>
+    <>
+      <Navbar height={700} width={{ sm: 300 }} p="md">
+        <Flex direction="column" align="center" justify="center">
+          <Link href="/">Home Page</Link>
+          <Link href="/products">Products</Link>
+          <Link href="/products/user-products">My Products</Link>
+          <Link href="/products/newProduct">Add Product</Link>
+          {authenticatedUser ? (
+            <Link href="/logout">Logout</Link>
+          ) : (
+            <Link href="/auth">Login</Link>
+          )}
+        </Flex>
+      </Navbar>
+    </>
   )
- }
+}

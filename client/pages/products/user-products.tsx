663b5ad89f4989101e94fb4b358588636ebd8a9c
sanjibpaul59 <sanjibpaul171@gmail.com>
2023-03-21 13:45:01 +0600

    Added navbar for moving around the application

--- client/pages/products/user-products.tsx
+++ client/pages/products/user-products.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import { Center, Flex, Button, Container } from '@mantine/core'
+import { Center, Flex, Button, Container, Grid } from '@mantine/core'
 import UserProduct from '@/components/products/UserProduct'
 import { Product } from '@/interfaces/Product'
 import { GetServerSideProps, NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
+import Navigation from '@/components/Navbar'
 
 interface UserProductsProps { 
   products: Product[]
@@ -37,27 +38,29 @@ const UserProducts: NextPage<Props> = ({ products }) => {
         <Head>
           <title>Products List</title>
         </Head>
-
+        <Grid>
+        <Grid.Col span="content">
+          <Navigation />
+          </Grid.Col>
+          <Grid.Col span={8}>
         <Center h={100}>
           <h1>MY PRODUCTS</h1>
         </Center>
-        {/* <p>{ products }</p> */}
-       
+
           {userProducts.map((product: any) => (
-           
-              // <Link href={`/products/${product.id}/edit-product`}>
-                <UserProduct product={product} />
-              // </Link>
-          
+              <UserProduct product={product} />
           ))}
-        
-        <Container size="40rem" mx="auto">
+             <Container size="40rem" mx="auto">
           <Flex justify="end" my={'lg'}>
             <Link href="/products/newProduct">
               <Button color="violet">ADD PRODUCT</Button>
             </Link>
           </Flex>
         </Container>
+          </Grid.Col>
+          </Grid>
+        
+        
       </>
     )
   }

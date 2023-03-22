797912ff0cc1bd319a1046599bb93d5702c5eeb2
sanjibpaul59 <sanjibpaul171@gmail.com>
2023-03-22 22:13:54 +0600

    unsold and available products along with sold, bought, lent and borrowed products controller

--- backend/app/controllers/products_controller.rb
+++ backend/app/controllers/products_controller.rb
@@ -13,16 +13,13 @@ class ProductsController < ApplicationController
     render json: @product.to_json(include: :categories)
   end
 
-  # POST /products
-  # def create
-  #   @product = Product.new(product_params)
+ # Get /unsold_products
+ def unsold_products
+    @products = Product.includes(:categories).unsold_and_available
+    render json: @products.to_json(include: :categories)
+  end
 
-  #   if @product.save
-  #     render json: @product.to_json(include: :categories), status: :created, location: @product
-  #   else
-  #     render json: @product.errors, status: :unprocessable_entity
-  #   end
-  # end
+  # POST /products
   def create
     @product = Product.new(product_params)
     category_ids = params[:category_ids]
--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -49,6 +49,51 @@ class UsersController < ApplicationController
     end
   end
 
+  def sold_products
+    user_id = params[:user_id]
+    if user_id.present?
+      @user = User.find(user_id)
+      # Select products from transactions where transaction_type is "buy"
+      @products = @user.sold_products.where(transactions: { transaction_type: "buy" })
+      render json: @products.includes(:categories).to_json(include: :categories)
+    else
+      render json: { error: "User id is required" }, status: :unprocessable_entity
+    end
+  end
+
+  def bought_products
+    user_id = params[:user_id]
+    if user_id.present?
+      @user = User.find(user_id)
+      @products = @user.bought_products.where(transactions: { transaction_type: "buy" })
+      render json: @products.includes(:categories).to_json(include: :categories)
+    else
+      render json: { error: "User id is required" }, status: :unprocessable_entity
+    end
+  end
+
+  def lent_products
+    user_id = params[:user_id]
+    if user_id.present?
+      @user = User.find(user_id)
+      @products = @user.lent_products.where(transactions: { transaction_type: "rent" })
+      render json: @products.includes(:categories).to_json(include: :categories)
+    else
+      render json: { error: "User id is required" }, status: :unprocessable_entity
+    end
+  end
+
+  def borrowed_products
+    user_id = params[:user_id]
+    if user_id.present?
+      @user = User.find(user_id)
+      @products = @user.borrowed_products.where(transactions: { transaction_type: "rent" })
+      render json: @products.includes(:categories).to_json(include: :categories)
+    else
+      render json: { error: "User id is required" }, status: :unprocessable_entity
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user

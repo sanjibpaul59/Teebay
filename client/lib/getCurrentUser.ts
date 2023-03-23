55123d4f1beb1dcf0b390085094dbe638a83293b
sanjibpaul59 <sanjibpaul171@gmail.com>
2023-03-23 11:43:47 +0600

    get current user in any page and remove styles for lists

--- client/lib/getCurrentUser.ts
+++ client/lib/getCurrentUser.ts
@@ -1,9 +1,15 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import axios from 'axios';
 
 export const getCurrentUser = () => { 
  const [currentUser, setCurrentUser] = useState<string | null>(null);
   useEffect(() => {
+    // const userData = async () => { 
+    //   const user = await axios.get('http://localhost:3000/current_user')
+    //   console.log(user)
+    //   setCurrentUser(user.data)
+    // }
     const userId = localStorage.getItem('userId')
     setCurrentUser(userId)
   }, []);

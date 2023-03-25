71354e37b09ee534ac19e0e06df193b555d56a66
sanjibpaul59 <sanjibpaul171@gmail.com>
2023-03-25 23:45:38 +0600

    Updated cors to accept request from any origin, configured the database to connect with postgres container and added seeds to get the app up and running

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = User.create([{
+ first_name: "Alexander",
+ last_name: "Supertramp",
+ address: "third world country",
+ email: "alexander1@mail.com",
+ phone_number: "01234567895",
+ password: "PassWhat??",
+}])
+
+products = Product.create([{
+ title: "The best product",
+ description: "This is the best product ever",
+ selling_price: 100,
+ rent_amount: 10,
+ rent_type: "daily",
+ user_id: 1,
+}])
+
+categories = Category.create([{
+ category_name: "electronics",
+},
+{
+ category_name: "furniture",
+},
+{
+ category_name: "home appliances",
+},
+{
+ category_name: "sporting goods",
+},
+{
+ category_name: "outdoor",
+},
+{
+ category_name: "toys",
+}
+])
+
+

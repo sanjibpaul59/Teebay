afd6be89c0115970e724499ea0772f7c22484221
sanjibpaul59 <sanjibpaul171@gmail.com>
2023-03-27 01:04:40 +0600

    Removed unused import and deleted it

--- client/pages/auth/registration.tsx
+++ client/pages/auth/registration.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router'
 import axios from 'axios'
 import { useState } from 'react'
 import RegistrationForm from '@/components/authentication/RegistrationForm'
-import api from '@/baseUrl'
+
 
 interface RegistrationResponse { 
   error?: string
